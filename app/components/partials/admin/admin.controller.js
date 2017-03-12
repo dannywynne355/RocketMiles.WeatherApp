9d782b2abbbb9e389eb13a3e38deb3246b8cffa2
Danny Wynne <danny@the355group.com>
2017-03-12 10:10:53 -0500

    Styling updates

--- app/components/partials/admin/admin.controller.js
+++ app/components/partials/admin/admin.controller.js
@@ -4,6 +4,12 @@ WeatherAppControllers.controller('AdminCtrl', AdminController);
 
 function AdminController($scope, broadcastEvents, appCookie) {
 
+    var cookie = appCookie.load();
+    $scope.cookieData = {
+        weatherUnits: cookie ? cookie.units : "undefined",
+        locations: cookie ? cookie.locations : []
+    };
+
     $scope.refresh = function () {
         // Request that the data if refreshed
         $scope.$emit(broadcastEvents.setLocation.refreshNotification);
