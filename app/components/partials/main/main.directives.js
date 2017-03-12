c2487da3749ec106e73986d1bd2591a0e6f3efa2
Danny Wynne <danny@the355group.com>
2017-03-12 14:05:19 -0500

    Add check on temp to handle difference between 0 deg and false

---
+++ app/components/partials/main/main.directives.js
@@ -0,0 +1,25 @@
+﻿/*
+var WeatherAppDirectives = angular.module('WeatherApp.directives');
+
+ WeatherAppDirectives.directive('routeLoadingIndicator', routeLoadingIndicator);
+
+var routeLoadingIndicator = function ($rootScope) {
+    return {
+        restrict: 'E',
+        template: "<div class='col-lg-12' ng-if='isWeatherLoading'><h1>Loading <i class='fa fa-cog fa-spin'></i></h1></div>",
+        link: function (scope, elem, attrs) {
+            scope.isWeatherLoading = false;
+
+            $rootScope.$on('$routeChangeStart', function () {
+                scope.isWeatherLoading = true;
+            });
+
+            $rootScope.$on('$routeChangeSuccess', function () {
+                scope.isWeatherLoading = false;
+            });
+        }
+    };
+};
+
+routeLoadingIndicator.$inject = ['$rootScope'];
+*/
