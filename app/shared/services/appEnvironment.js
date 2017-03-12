519a20bdcd55b1e6a518a025663216465c008ff6
Danny Wynne <danny@the355group.com>
2017-03-12 16:48:29 -0500

    Clean up

---
+++ app/shared/services/appEnvironment.js
@@ -0,0 +1,29 @@
+﻿/*
+    Provides interface to determine the environment for the App.
+    Possibilities include Development, Staging, and Production.
+    Uses the host name of the App to determine environment
+ */
+angular.module('WeatherApp.services')
+    .factory('appEnvironment', appEnvironment);
+
+appEnvironment.$inject = ['$location', 'appEnvironmentSettings'];
+
+function appEnvironment($location, appEnvironmentSettings) {
+    var svc = {};
+
+    /* Returns an object containing the environment settings */
+    svc.get = function () {
+        if (appEnvironmentSettings.development
+            && appEnvironmentSettings.development.hosts.indexOf($location.host()) > -1) {
+            return appEnvironmentSettings.development;
+        } else if (appEnvironmentSettings.staging
+            && appEnvironmentSettings.staging.hosts.indexOf($location.host()) > -1) {
+            return appEnvironmentSettings.staging;
+        } else {
+            return appEnvironmentSettings.production;
+        }
+        ;
+    };
+
+    return svc;
+}
